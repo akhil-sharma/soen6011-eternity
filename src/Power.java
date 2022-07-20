42bd2b1926f55c1ff6894d0435bbd95f48264397
Akhil Sharma <akhilsharma.cse@gmail.com>
2022-07-19 23:16:00 -0400

    WIP: add initial condition check for calculating the power method.

--- src/Power.java
+++ src/Power.java
@@ -1,7 +1,7 @@
 class Power {
 
-    private double power(int base, int power) {
-        int solution = 1;
+    private double powerRepeatedMultiply(double base, double power) {
+        double solution = 1;
         for (int i = 0; i < power; i++) {
             solution *= base;
         }
@@ -9,7 +9,28 @@ class Power {
         return solution;
     }
 
+    private double checkedPower(double base, int power) {
+        if (power == 0) {
+            return 1;
+        }
+
+        if (power == 1) {
+            return base;
+        }
+
+        if (power < 0) {
+            return 1 / powerRepeatedMultiply(base, -power);
+        }
+
+        return powerRepeatedMultiply(base, power);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Power().power(2, 3));
+        System.out.println(new Power().checkedPower(2, 3));
+        System.out.println(new Power().checkedPower(2, -1));
+        System.out.println(new Power().checkedPower(2, 0));
+        System.out.println(new Power().checkedPower(0, 0));
+        System.out.println(new Power().checkedPower(2, -2));
     }
 }
