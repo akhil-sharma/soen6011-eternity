951200f23147d4fd2e0fb2c58a377ca08f6cb49c
Akhil Sharma <akhilsharma.cse@gmail.com>
2022-07-22 17:19:17 -0400

    feat: adds error messages to ErrorString.java
    improves modularity and enables internationalization

---
+++ src/ErrorStrings.java
@@ -0,0 +1,8 @@
+public class ErrorStrings {
+    public static String UNKNOWN_COMMAND_ERROR = "Unknown command. No action was performed.";
+    public static String INVALID_COMMAND_ERROR = "We have encountered an invalid command.\nTry again:";
+    public static String BAD_ARGUMENT_COUNT_POWER = ":pow command requires 2 agruments, base and power.\nUSAGE> :pow x y\nexample (2^3)> :pow 2 3";
+    public static String FIRST_ARGUMENT_NOT_NUM_POWER = "The first argument of :pow must be a number.\nUSAGE> :pow x y\nexample (2^3)> :pow 2 3";
+    public static String SECOND_ARGUMENT_NOT_NUM_POWER = "The second argument of :pow must be an integer.\nUSAGE> :pow x y\nexample (2^3)> :pow 2 3";
+    public static String ZERO_BASE_NEGATIVE_POWER = "Base cannot be 0 when power is negative.";
+}
