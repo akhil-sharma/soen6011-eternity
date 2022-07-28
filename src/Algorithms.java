d69968a210514d01759ea399c86673a5a6a755a9
Akhil Sharma <akhilsharma.cse@gmail.com>
2022-07-28 18:10:11 -0400

    feat: adds junit tests for Algorithms.java
    all test are passing

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -8,19 +8,23 @@ public class Algorithms {
   Algorithms() {
   }
 
-  /*
+  /**
    * Expects the command in the for of String[].
    * 
+   * <p>
    * A valid command looks like [":pow", "x", "y"]
    * where x is a double and y is an integer.
    * 
    * <bold> y will be casted to integer if it is a double.</bold>
    * 
+   * <p>
    * This method
    * <ol>
-   * <li> Validates the arguments provided by the user.</li>
-   * <li> Converts the arguments to appropriate types.</li>
+   * <li>Validates the arguments provided by the user.</li>
+   * <li>Converts the arguments to appropriate types.</li>
    * </ol>
+   * 
+   * @param command An array of Strings representing the user command
    */
   public void power(String[] command) {
     // Check that :pow gets 2 arguments
@@ -46,21 +50,21 @@ public class Algorithms {
       yTemp = Double.parseDouble(command[2]);
 
       if (Math.ceil(yTemp) != Math.floor(yTemp)) {
-        System.out.println(ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+        System.err.println(ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
         return;
       }
 
       y = (int) yTemp;
 
     } catch (NumberFormatException nfe) {
-      System.err.println(ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+      System.err.print(ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
       return;
     }
 
     // adjust for negative power.
     if (y < 0) {
       if (x == 0) {
-        System.out.println(ErrorStrings.ZERO_BASE_NEGATIVE_POWER);
+        System.err.println(ErrorStrings.ZERO_BASE_NEGATIVE_POWER);
         return;
       }
       System.out.println(1 / powerDivideAndConquer(x, -y));
--- src/AlgorithmsTest.java
+++ src/AlgorithmsTest.java
@@ -1,17 +1,168 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AlgorithmsTest {
 
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
   private Algorithms testAlg;
 
+  /**
+   * 
+   * @throws Exception
+   */
   @Before
   public void setUp() throws Exception {
     testAlg = new Algorithms();
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
   }
 
   @Test
-  public void testPower() throws Exception {
+  public void testPowerBadLength() throws Exception {
+    final String[] testInput1 = { ":pow", "2" }; // short command
+    final String[] testInput2 = { ":pow" }; // short command
+    final String[] testInput3 = { ":pow", "3", "4", "5" }; // long command
+    final String[] testInput4 = { ":pow", "2", "3" }; // correct command length
 
+    testAlg.power(testInput1);
+    assertEquals(errContent.toString().trim(), ErrorStrings.BAD_ARGUMENT_COUNT_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(errContent.toString().trim(), ErrorStrings.BAD_ARGUMENT_COUNT_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput3);
+    assertEquals(errContent.toString().trim(), ErrorStrings.BAD_ARGUMENT_COUNT_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput4);
+    assertEquals(outContent.toString().trim(), "8.0");
+    outContent.reset();
+  }
+
+  /**
+   * 
+   * @throws Exception
+   */
+  @Test
+  public void testPowerFirstArgBad() throws Exception {
+    final String[] testInput1 = { ":pow", "2", "3" }; // correct command
+    final String[] testInput2 = { ":pow", "a", "3" }; // bad first argument
+    final String[] testInput3 = { ":pow", "a long string", "5" }; // bad first argument
+    final String[] testInput4 = { ":pow", "f", "4" }; // bad first argument
+
+    testAlg.power(testInput1);
+    assertEquals(outContent.toString().trim(), "8.0");
+    outContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(errContent.toString().trim(), ErrorStrings.FIRST_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput3);
+    assertEquals(errContent.toString().trim(), ErrorStrings.FIRST_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput4);
+    assertEquals(errContent.toString().trim(), ErrorStrings.FIRST_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+  }
+
+  @Test
+  public void testPowerSecondArgBad() throws Exception {
+    final String[] testInput1 = { ":pow", "2", "3" }; // correct command
+    final String[] testInput2 = { ":pow", "4", "a" }; // bad second argument
+    final String[] testInput3 = { ":pow", "6", "p" }; // bad second argument
+    final String[] testInput4 = { ":pow", "40", "wt" }; // bad second argument
+
+    testAlg.power(testInput1);
+    assertEquals(outContent.toString().trim(), "8.0");
+    outContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(errContent.toString().trim(), ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput3);
+    assertEquals(errContent.toString().trim(), ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput4);
+    assertEquals(errContent.toString().trim(), ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+  }
+
+  @Test
+  public void testPowerSecondArgNotInt() throws Exception {
+    final String[] testInput1 = { ":pow", "2", "3" }; // correct command
+    final String[] testInput2 = { ":pow", "4", "3.1" }; // bad second argument
+
+    testAlg.power(testInput1);
+    assertEquals(outContent.toString().trim(), "8.0");
+    outContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(errContent.toString().trim(), ErrorStrings.SECOND_ARGUMENT_NOT_NUM_POWER);
+    errContent.reset();
+  }
+
+  @Test
+  public void testPowerZeroBaseNegPow() throws Exception {
+    final String[] testInput1 = { ":pow", "2", "3" }; // correct command
+    final String[] testInput2 = { ":pow", "0", "2" }; // correct argument
+    final String[] testInput3 = { ":pow", "0", "-1" }; // bad second argument
+    final String[] testInput4 = { ":pow", "0", "-2" }; // bad second argument
+
+    testAlg.power(testInput1);
+    assertEquals(outContent.toString().trim(), "8.0");
+    outContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(outContent.toString().trim(), "0.0");
+    outContent.reset();
+
+    testAlg.power(testInput3);
+    assertEquals(errContent.toString().trim(), ErrorStrings.ZERO_BASE_NEGATIVE_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput4);
+    assertEquals(errContent.toString().trim(), ErrorStrings.ZERO_BASE_NEGATIVE_POWER);
+    errContent.reset();
+  }
+
+  @Test
+  public void testPowerNegPower() throws Exception {
+    final String[] testInput1 = { ":pow", "2", "-1" };
+    final String[] testInput2 = { ":pow", "0", "-1" };
+    final String[] testInput3 = { ":pow", "5", "-1" };
+
+    testAlg.power(testInput1);
+    assertEquals(outContent.toString().trim(), "0.5");
+    outContent.reset();
+
+    testAlg.power(testInput2);
+    assertEquals(errContent.toString().trim(), ErrorStrings.ZERO_BASE_NEGATIVE_POWER);
+    errContent.reset();
+
+    testAlg.power(testInput3);
+    assertEquals(outContent.toString().trim(), "0.2");
+    outContent.reset();
   }
 }
